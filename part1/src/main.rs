8fdfcfdeb1112f853e06b434d7b0500d036a40de
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2019-02-05 18:23:02 -0800

    Code Review and Suggestions for improvement

--- part1/src/main.rs
+++ part1/src/main.rs
@@ -2,107 +2,90 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("error: unable to read user input");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("error: unable to read user input");
 
-    println!("{}", Interpreter::new(input).expr());
+    println!("{}", Interpreter::new(input.trim()).expr());
 }
 
 struct Interpreter {
     input: String,
-    position: i64,
+    position: usize,
     current_token: Option<Token>,
 }
 
 impl Interpreter {
-    pub fn new(input: String) -> Self {
-        Interpreter{input, position: 0, current_token: None}
+    pub fn new(input: &str) -> Self {
+        Interpreter {
+            input: input.to_string(),
+            position: 0,
+            current_token: None,
+        }
     }
 
     fn next_token(&mut self) {
-        if &self.position >= &(&self.text_length() - 1) {
-            self.current_token = Some(Token{token_value: TokenValue::NONE, token_type: TokenType::EOF});
-            return ();
+        if self.position >= self.text_length() {
+            eprintln!(
+                "position: {}; text_length: {}",
+                self.position,
+                self.text_length()
+            );
+            self.current_token = Some(Token::NONE);
+            return;
         }
 
-        let current_char: String = self.input
-            .chars()
-            .skip(self.position as usize)
-            .take(1)
-            .collect();
+        let current_char: String = self.input.chars().skip(self.position).take(1).collect();
 
         self.position += 1;
 
         if let Ok(current_char_as_int) = current_char.parse::<i64>() {
-            self.current_token = Some(Token{token_value: TokenValue::INT(current_char_as_int), token_type: TokenType::INT});
-            return ();
+            self.current_token = Some(Token::INT(current_char_as_int));
+            return;
         }
 
         if current_char == "+" {
-            self.current_token = Some(Token{token_value: TokenValue::OPERATION(Operation::PLUS), token_type: TokenType::OPERATION});
-            return ();
+            self.current_token = Some(Token::OPERATION(Operation::PLUS));
+            return;
         }
 
-        panic!("Wrong Character");
+        panic!("Wrong Character: {}", current_char);
     }
 
-    fn text_length(&self) -> i64 {
-        *&self.input.len() as i64
-    }
-
-    fn eat(&mut self, token_type: TokenType) {
-        let current_type = self.current_token.take().unwrap();
-
-        if current_type.is_equal(&token_type) {
-            self.next_token();
-            return ();
-        }
-
-        panic!("Wrong type");
+    fn text_length(&self) -> usize {
+        self.input.len()
     }
 
     pub fn expr(&mut self) -> i64 {
         self.next_token();
 
-        let left = self.current_token.clone().unwrap();
-        self.eat(TokenType::INT);
+        let left = self.current_token.unwrap();
+        self.next_token();
 
-        let _operation = self.current_token.clone().unwrap();
-        self.eat(TokenType::OPERATION);
+        if let Token::OPERATION(_) = self.current_token.unwrap() {
+            self.next_token();
+        } else {
+            panic!("Expected operation, found {:?}", self.current_token);
+        }
 
-        let right = self.current_token.clone().unwrap();
-        self.eat(TokenType::INT);
+        let right = self.current_token.unwrap();
+        self.next_token();
 
         left.into_int() + right.into_int()
     }
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Token {
-    token_value: TokenValue,
-    token_type: TokenType,
-}
-
 impl Token {
-    pub fn is_equal(&self, token_type: &TokenType) -> bool {
-        std::mem::discriminant(&self.token_type) == std::mem::discriminant(token_type)
-    }
-
     pub fn into_int(self) -> i64 {
-        if let TokenValue::INT(value) = self.token_value {
+        if let Token::INT(value) = self {
             return value;
         }
 
-        panic!("Value not integer.");
+        panic!("Value not integer: {:#?}", self);
     }
 }
 #[derive(Debug, Copy, Clone)]
-enum TokenType {
-    INT,
-    OPERATION,
-    EOF,
-}
-#[derive(Debug, Copy, Clone)]
-enum TokenValue {
+enum Token {
     INT(i64),
     OPERATION(Operation),
     NONE,
@@ -110,4 +93,21 @@ enum TokenValue {
 #[derive(Debug, Copy, Clone)]
 enum Operation {
     PLUS,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(Interpreter::new("1+2").expr(), 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn int_where_operator_should_be_fails() {
+        Interpreter::new("123").expr();
+    }
+
 }
