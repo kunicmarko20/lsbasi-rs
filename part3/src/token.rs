b59c09ab19de996a1a2e263ddbeacb093013c189
kunicmarko20 <kunicmarko20@gmail.com>
2019-02-12 19:23:03 +0100

    part3 - multiplication and divison

---
+++ part3/src/token.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, Copy, Clone)]
+pub enum Token {
+    INT(i64),
+    OPERATION(Operation),
+    NONE,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Operation {
+    ADDITION,
+    SUBTRACTION,
+    MULTIPLICATION,
+    DIVISION,
+}
+
+impl Token {
+    pub fn into_int(self) -> Result<i64, String> {
+        if let Token::INT(value) = self {
+            return Ok(value);
+        }
+
+        Err(String::from("Value not integer."))
+    }
+}
